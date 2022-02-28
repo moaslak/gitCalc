e0bbbf670e850d436bf83ee6e7191a7db36f2bb5
moaslak <moaslak@gmail.com>
2022-02-28 13:46:05 +0100

    Revert "Merge branch 'main' of https://github.com/moaslak/gitCalc"
    This reverts commit 0712deaff1e4530ff8f80eab9397a95491ba7f81, reversing
    changes made to f2efbf675ef68dee8e83da45433d96cef450a40c.

--- gitClac/gitClac/Program.cs
+++ gitClac/gitClac/Program.cs
@@ -1,15 +1,2 @@
-﻿using System;
-
-namespace gitClac; // Note: actual namespace depends on the project name.
-
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        while (true)
-        {
-            menuClass menu = new menuClass();
-        }
-        
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
