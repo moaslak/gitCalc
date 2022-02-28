46683cc57fdc0bef1c28e116fbdd2762e4f3dffa
moaslak <moaslak@gmail.com>
2022-02-28 13:08:05 +0100

    calc implement

--- gitClac/gitClac/menuClass.cs
+++ gitClac/gitClac/menuClass.cs
@@ -4,27 +4,52 @@ namespace gitClac;
 
 public class menuClass
 {
-    public double Tal1 { get; set; }
-    public double Tal2 { get; set; }
-
+    double Tal1;
+    double Tal2;
     public menuClass()
     {
         int operation = 0;
         Calc calc = new Calc();
+        Console.Write("Select mode: ");
         
+        Int32.TryParse(Console.ReadLine(), out operation);
             switch (operation)
           {
             case 1:
-                calc.plus(Tal1, Tal1);
+                Console.Write("Input Tal1; ");
+                double.TryParse(Console.ReadLine(), out Tal1);
+                Console.Write("Input Tal2; ");
+                double.TryParse(Console.ReadLine(), out Tal2);
+
+                Console.WriteLine(calc.plus(Tal1, Tal2).ToString());
                 break;
             case 2:
-                calc.minus(Tal1, Tal1);
+                Console.Write("Input Tal1; ");
+                double.TryParse(Console.ReadLine(), out Tal1);
+                Console.Write("Input Tal2; ");
+                double.TryParse(Console.ReadLine(), out Tal2);
+
+                Console.WriteLine(calc.minus(Tal1, Tal2).ToString());
                 break;
             case 3:
-                calc.multiply(Tal1, Tal1);
+                Console.Write("Input Tal1; ");
+                double.TryParse(Console.ReadLine(), out Tal1);
+                Console.Write("Input Tal2; ");
+                double.TryParse(Console.ReadLine(), out Tal2);
+
+                Console.WriteLine(calc.multiply(Tal1, Tal2).ToString());
+                
                 break;
             case 4:
-                calc.div(Tal1, Tal1);
+                Console.Write("Input Tal1; ");
+                double.TryParse(Console.ReadLine(), out Tal1);
+                Console.Write("Input Tal2; ");
+                double.TryParse(Console.ReadLine(), out Tal2);
+
+                Console.WriteLine(calc.div(Tal1, Tal2).ToString());
+                break;
+            default:
+                Console.WriteLine("1, 2, 3, 4");
                 break;
             }
     }
